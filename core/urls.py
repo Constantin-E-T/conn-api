55b61b1a04dc38f414770033ef00318633e0b264
Constantin ET <57395630+Constantin-E-T@users.noreply.github.com>
2025-03-10 05:29:27 +0000

    added some endpoints for testing and created a new app tasks.

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Get host from request or environment
 schema_view = get_schema_view(
    openapi.Info(
       title="Django API",
@@ -18,14 +17,15 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
    authentication_classes=[],
-   validators=[],  # Ignore validators to avoid validation errors
+   validators=[],
 )
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('api/', include('api.urls')),
+    path('tasks/', include('tasks.urls')),
     
-    # Swagger UI URLs - simpler path patterns
+    # Swagger UI URLs
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
